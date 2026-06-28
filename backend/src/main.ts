90cea91fd6015830be0673f830191a114cb8c088
Taggconr <deprofie082@gmail.com>
2026-06-28 21:35:35 +0300

    back reset commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,15 +4,14 @@ import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  app.use(cookieParser());
-  app.enableCors({
-    origin: 'https://museum-web-lyguhs-projects.vercel.app/',
-    //origin: configService.get('CORS_ORIGIN'), // адрес фронта
-    //origin: 'http://localhost:3000',
-    credentials: true,
-  });
-  await app.listen(9000);
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    app.use(cookieParser());
+    app.enableCors({
+        origin: configService.get('CORS_ORIGIN'), // адрес фронта
+        //origin: 'http://localhost:3000',
+        credentials: true,
+    });
+    await app.listen(9000);
 }
 bootstrap();
